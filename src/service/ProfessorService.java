e5046c76bdd88e1d5fa865207d01cdcdc3f2feb6
arrudapointexe <itallo.mccg@gmail.com>
2025-08-11 11:11:49 -0300

    att ProfessorService.java

--- src/service/ProfessorService.java
+++ src/service/ProfessorService.java
@@ -1,65 +1,42 @@
-public class ProfessorService extends Pessoa {
-    // Atributos
-    private Disciplina[] disciplinas; // Substituído
-    private int numDisciplinas;       // Contador manual
-    private static final int MAX_DISCIPLINAS = 10; // Tamanho máximo fixo
+public class ProfessorService {
 
-    public ProfessorService(String nome) {
-        super(nome);
-        // Inicializa o array com o tamanho máximo e o contador com zero
-        this.disciplinas = new Disciplina[MAX_DISCIPLINAS];
-        this.numDisciplinas = 0;
+    // --- Métodos ---
+
+    public void solicitarBolsa(Professor professor, Disciplina disciplina, String tipoBolsa) {
+        System.out.println("LOGICA DE SERVICO: Prof(a). " + professor.getNome() + 
+                           " solicitou uma bolsa " + tipoBolsa + 
+                           " para a disciplina " + disciplina.getNome() + ".");
+        // Aqui entraria a comunicação com  a classe PROGRAD
     }
 
-    // Método para adicionar disciplina, agora com controle de capacidade
-    public void adicionarDisciplina(Disciplina disciplina) {
-        if (this.numDisciplinas < MAX_DISCIPLINAS) {
-            this.disciplinas[this.numDisciplinas] = disciplina;
-            this.numDisciplinas++;
-            disciplina.setProfessorOrientador(this);
-        } else {
-            System.out.println("Erro: Limite de disciplinas para o professor " + this.nome + " atingido.");
-        }
+    public void indicarMonitor(Professor professor, Disciplina disciplina, AlunoMonitor monitor) {
+        disciplina.setMonitor(monitor);
+        monitor.setDisciplina(disciplina);
+        monitor.setProfessor(professor);
+        System.out.println("LOGICA DE SERVICO: O aluno " + monitor.getNome() + 
+                           " foi indicado como monitor de " + disciplina.getNome() + 
+                           " pelo(a) Prof(a). " + professor.getNome() + ".");
     }
 
-    // Método de acompanhamento modificado para usar o contador manual
-    public void acompanharMonitorias() {
-        System.out.println("\n--- Acompanhamento de Monitorias - Prof(a). " + this.nome + " ---");
-        
-        for (int i = 0; i < this.numDisciplinas; i++) {
-            Disciplina disc = this.disciplinas[i];
+    public void cadastrarHorarioAtendimento(Disciplina disciplina, String dia, String hora, String local) {
+        String novoHorario = dia + " das " + hora + " no local: " + local;
+        disciplina.adicionarHorario(novoHorario); // Supondo que Disciplina tenha este método
+        System.out.println("LOGICA DE SERVICO: Novo horário de atendimento para " + 
+                           disciplina.getNome() + " cadastrado: " + novoHorario);
+    }
+
+    public void acompanharMonitorias(Professor professor) {
+        System.out.println("\n--- Acompanhamento de Monitorias - Prof(a). " + professor.getNome() + " ---");
+        for (Disciplina disc : professor.getDisciplinas()) {
             System.out.println("Disciplina: " + disc.getNome());
             AlunoMonitor monitor = disc.getMonitor();
             if (monitor != null) {
                 System.out.println("  - Monitor Designado: " + monitor.getNome());
+                System.out.println("  - Horários Cadastrados: " + disc.getHorariosAtendimento().size());
             } else {
                 System.out.println("  - Nenhum monitor designado ainda.");
             }
         }
         System.out.println("-----------------------------------------------------");
-    }
-
-    // --- Outros métodos permanecem iguais ---
-
-    public void solicitarBolsa(Disciplina disciplina, String tipoBolsa, int quantidade) {
-        System.out.println("Prof(a). " + this.nome + " está solicitando " + quantidade + " bolsa(s) para " + disciplina.getNome() + "...");
-        try {
-            PROGRAD prograd = PROGRAD.getInstance();
-            prograd.concederBolsa(disciplina.getDepartamento(), quantidade);
-        } catch (BolsaException e) {
-            System.out.println("!!! FALHA NA SOLICITAÇÃO: " + e.getMessage());
-        }
-    }
-    
-    @Override
-    public void apresentar() {
-        System.out.println("Sou o Professor Orientador: " + this.nome);
-    }
-    
-    public void indicarMonitor(Disciplina d, AlunoMonitor m) { 
-        d.setMonitor(m);
-        m.setDisciplina(d);
-        m.setProfessor(this);
-        System.out.println("O aluno " + m.getNome() + " foi indicado como monitor de " + d.getNome() + ".");
     }
 }
