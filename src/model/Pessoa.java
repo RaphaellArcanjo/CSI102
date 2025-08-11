c01bd2e0e6a4c31f640feeff80dcb877ebde216e
arrudapointexe <itallo.mccg@gmail.com>
2025-08-10 22:31:27 -0300

    add Pessoa.java

---
+++ src/model/Pessoa.java
@@ -0,0 +1,18 @@
+public abstract class Pessoa {
+    protected String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Método abstrato que força as classes filhas a se apresentarem
+    public abstract void apresentar();
+}
